db214a0f3e42cd1205e4e27c80cfc2f718d77528
m00zy <maxwellzhu123@gmail.com>
2024-08-09 00:35:23 +1200

    setup Square component

--- chessGame/src/App.jsx
+++ chessGame/src/App.jsx
@@ -1,41 +1,9 @@
 import './App.css'
-import { useState } from 'react';
 
-function Square({piece}){
+export default function Square(){
   return (
     <div className="square">
-      {piece}  
+      1
     </div>
   );
-}
-
-function Board(){
-  return (
-    <div className="board">
-        <div className="board-row">
-        <button className="square">1</button>
-        <button className="square">2</button>
-        <button className="square">3</button>
-      </div>
-      <div className="board-row">
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square">6</button>
-      </div>
-      <div className="board-row">
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
-      </div>
-    </div>
-  )
-}
-
-export default function ChessGame(){
-  return (
-    <div>
-      <h1 className="game-banner">Chess Game</h1>
-      <Board />
-    </div>
-  )
 }
