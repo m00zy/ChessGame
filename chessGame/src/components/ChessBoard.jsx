d1c955d054f0410af36c78c085bf11c1a39b8a70
m00zy <maxwellzhu123@gmail.com>
2024-08-14 20:32:15 +1200

    improve styling by adding row and column labels

--- chessGame/src/components/ChessBoard.jsx
+++ chessGame/src/components/ChessBoard.jsx
@@ -13,13 +13,36 @@ function ChessBoard() {
         ['♖', '♘', '♗', '♕', '♔', '♗', '♘', '♖'],
     ];
 
+    const columnLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+    const rowNumbers = ['1', '2', '3', '4', '5', '6', '7', '8'];
+
     return (
-        <div className='ChessBoard'>
+        <div className='left'>
+            <div className='row-labels'>
             {
-                chessGrid.map((rowPieces) => {
-                    return <ChessRow rowPieces={rowPieces} />
+                rowNumbers.map((number) => {
+                    return <div className='label'>{number}</div>;
                 })
             }
+            </div>
+            <div className='main'>
+                <div className='ChessBoard'>
+                    <div className="board">
+                        {
+                            chessGrid.map((rowPieces) => {
+                                return <ChessRow rowPieces={rowPieces} />;
+                            })
+                        }
+                    </div>
+                    <div className='column-labels'>
+                        {
+                            columnLetters.map((letter) => {
+                                return <div className='label'>{letter}</div>;
+                            })
+                        }
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
