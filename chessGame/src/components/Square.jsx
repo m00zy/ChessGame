9804e11bbe161c6bb1040cc18043bfe97e0d69b4
m00zy <maxwellzhu123@gmail.com>
2024-08-15 02:07:42 +1200

    improve chess piece icons

--- chessGame/src/components/Square.jsx
+++ chessGame/src/components/Square.jsx
@@ -1,9 +1,28 @@
 import '../stylesheets/Square.css';
+import k from '../assets/pieces/K.png';
+import q from '../assets/pieces/Q.png';
+import b from '../assets/pieces/B.png';
+import n from '../assets/pieces/N.png';
+import r from '../assets/pieces/R.png';
+import p from '../assets/pieces/P.png';
 
 function Square({ piece }){
+
+  const pieceImages = {
+    K: k,
+    Q: q,
+    B: b,
+    N: n,
+    R: r,
+    P: p,
+  };
+
+  const pieceColor = piece[0] === 'W' ? 'white-piece' : 'black-piece';
+  const pieceType = piece[1];
+
     return (
       <div className="square">
-        {piece}
+        <img src={pieceImages[pieceType]} alt={piece} className={pieceColor}/>
       </div>
     );
   }
